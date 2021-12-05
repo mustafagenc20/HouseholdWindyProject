c1921dfeec26e5dcc99961d7b9ac2aa6c1ad3b96
Mustafa Genc <mustafagenc20@gmail.com>
2021-12-05 21:40:21 +0100

    initial commit

--- src/main/java/be/intecbrussel/householdwindyproject/service/VacuumCleaner.java
+++ src/main/java/be/intecbrussel/householdwindyproject/service/VacuumCleaner.java
@@ -1,5 +1,7 @@
 package be.intecbrussel.householdwindyproject.service;
 
+import be.intecbrussel.householdwindyproject.service.enums.Tool;
+
 public class VacuumCleaner implements Tool {
     @Override
     public void doCleaningJob() {
